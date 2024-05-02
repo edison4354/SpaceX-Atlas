dc43be4d51d24df0520d1a39e70df644893eabed
Edison <edison4354@gmail.com>
2024-05-02 11:11:28 -0700

    Add 3d three.js globe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import Example from "./scripts/example";
 import Second from "./scripts/second";
+import Globe from 'globe.gl';
 
 Example();
 Second();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const globe = Globe()
+    .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg')
+    (document.getElementById('globeViz'));
+
+    globe.render();
+})
